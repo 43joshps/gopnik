c55ed709eacf7bc2208f3a0f456afd43a976505d
43joshps <133405316+43joshps@users.noreply.github.com>
2023-05-12 14:56:56 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,76 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+<!DOCTYPE html>
+<html>
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+<head>
+  <title>Rainbow Background with Hammer and Sickle</title>
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+      background-color: #FF0000;
+      animation: rainbow 0.4s linear infinite;
+    }
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+    @keyframes rainbow {
+      0% {
+        background-color: #FF0000;
+      }
 
-      <Footer />
-    </div>
-  )
-}
+      16.67% {
+        background-color: #FF7F00;
+      }
+
+      33.33% {
+        background-color: #FFFF00;
+      }
+
+      50% {
+        background-color: #00FF00;
+      }
+
+      66.67% {
+        background-color: #0000FF;
+      }
+
+      83.33% {
+        background-color: #4B0082;
+      }
+
+      100% {
+        background-color: #8F00FF;
+      }
+    }
+
+    #hammer-and-sickle {
+      position: absolute;
+      top: 10px;
+      left: 10px;
+      width: 150px;
+      height: 150px;
+      background-image: url("https://upload.wikimedia.org/wikipedia/commons/4/42/Portuguese_Communist_Party_Hammer_and_Sickle.png");
+      background-size: contain;
+      background-repeat: no-repeat;
+    }
+
+    #center-text {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      font-family: Impact, sans-serif;
+      font-size: 74px;
+      text-align: center;
+      color: #FFFFFF;
+      text-shadow: 2px 2px 4px #000000;
+      padding: 10px;
+      border-radius: 10px;
+    }
+  </style>
+</head>
+
+<body>
+  <div id="hammer-and-sickle"></div>
+  <div id="center-text">THE GOPNIKS HAVE TAKEN OVER</div>
+</body>
+
+</html>
